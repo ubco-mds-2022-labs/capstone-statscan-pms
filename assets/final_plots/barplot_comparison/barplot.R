2ea9a19b687813c3c97878ee313f0c8aad895c35
jonahedmundson <jonahedmundson>
2023-06-07 14:19:33 -0700

    comparison barplots

--- assets/final_plots/barplot_comparison/barplot.R
+++ assets/final_plots/barplot_comparison/barplot.R
@@ -45,35 +45,35 @@ cut_offs_mclust = list(
   PMS_prox_idx_parks = c(0.08254624, 0.01594993, 0.03244996, 0.04634999, 0.06235000, 0.08445002, 0.12194994),
   PMS_prox_idx_transit = c(0.001048913, 0.011649895)
 )
-cut_offs_kmeans_pam = list(
-  PMS_prox_idx_emp = c(0.003549657),
-  PMS_prox_idx_pharma = c(0.02634995),
-  PMS_prox_idx_childcare = c(0.03644997),
-  PMS_prox_idx_health = c(0.002249469),
-  PMS_prox_idx_grocery = c(0.01129957, 0.01894993, 0.02754996, 0.03894998, 0.05744995, 0.09185003, 0.16739932),
-  PMS_prox_idx_educpri = c(0.08265002),
-  PMS_prox_idx_educsec = c(0.05569919, 0.09904984, 0.17829930),
-  PMS_prox_idx_lib = c(0.09434931),
-  PMS_prox_idx_parks = c(0.04504995),
-  PMS_prox_idx_transit = c(0.007649841)
+cut_offs_quintile = list(
+  PMS_prox_idx_emp = c(0.0004, 0.0030, 0.0127, 0.0368),
+  PMS_prox_idx_pharma = c(0.0098, 0.0193, 0.0341, 0.0641),
+  PMS_prox_idx_childcare = c(0.0152, 0.0348, 0.0636, 0.1167),
+  PMS_prox_idx_health = c(0.0007, 0.0032, 0.0074, 0.0184),
+  PMS_prox_idx_grocery = c(0.0221, 0.0348, 0.0555, 0.0985),
+  PMS_prox_idx_educpri = c(0.0416, 0.0720, 0.1105, 0.1720),
+  PMS_prox_idx_educsec = c(0.0421, 0.0586, 0.0910, 0.1492),
+  PMS_prox_idx_lib = c(0.0558, 0.0707, 0.0960, 0.1488),
+  PMS_prox_idx_parks = c(0.0203, 0.0372, 0.0614, 0.1050),
+  PMS_prox_idx_transit = c(0.0026, 0.0067, 0.0131, 0.0272)
 )
-cut_offs_mixall = list(
-  PMS_prox_idx_emp = c(0.003649668),
-  PMS_prox_idx_pharma = c(0.02654995),
-  PMS_prox_idx_childcare = c(0.03634996),
-  PMS_prox_idx_health = c(0.002549527),
-  PMS_prox_idx_grocery = c(0.04844996),
-  PMS_prox_idx_educpri = c(0.08565002),
-  PMS_prox_idx_educsec = c(0.06324952, 0.14094998),
-  PMS_prox_idx_lib = c(0.09929952),
-  PMS_prox_idx_parks = c(0.04474999),
-  PMS_prox_idx_transit = c(0.00654981)
+cut_offs_hdbscan = list(PMS_prox_idx_emp = 0.22985, PMS_prox_idx_pharma = c(0.01175, 0.0525), PMS_prox_idx_childcare = 0.009, PMS_prox_idx_health = 0.1052,PMS_prox_idx_grocery = c(0.0763, 0.0124), PMS_prox_idx_educpri = c(0.04495,0.22045, 0.1449), PMS_prox_idx_educsec = c(0.0576, 0.0863), PMS_prox_idx_lib = c(0.06575, 0.0691, 0.05465), PMS_prox_idx_parks = 0.01995,PMS_prox_idx_transit = 0.03875)
+cut_offs_manual = list(
+  PMS_prox_idx_emp = c(0.0000423603, 0.0001573125, 0.0002743458, 0.0005010775),
+  PMS_prox_idx_pharma = c(0.01142592, 0.01948168),
+  PMS_prox_idx_childcare = c(0.008407409, 0.013850871),
+  PMS_prox_idx_health = c(0.0000405736, 0.0001524638, 0.0002658077),
+  PMS_prox_idx_grocery = c(0.01208361, 0.01846220),
+  PMS_prox_idx_educpri = c(0.04965835, 0.08169011),
+  PMS_prox_idx_educsec = c(0.06609424),
+  PMS_prox_idx_lib = c(0.6149259),
+  PMS_prox_idx_parks = c(0.01834893, 0.02944933),
+  PMS_prox_idx_transit = c(0.0000390986, 0.0001481958, 0.0002512994, 0.0003514904)
 )
-all_cutoffs = list(cut_offs_mclust, cut_offs_kmeans_pam, cut_offs_mixall)
+all_cutoffs = list(cut_offs_mclust, cut_offs_quintile, cut_offs_hdbscan, cut_offs_manual)
+all_cutoffs = lapply(all_cutoffs, function(x) lapply(x,sort))
 
-approaches = c('MCLUST', 'PAM k-means', 'MixAll')
-
-nice_colors = rev(brewer.pal(9, "YlGnBu"))
+approaches = c('MCLUST', 'Quintile', 'HDBSCAN', 'Min/Max')
 
 
 
@@ -89,15 +89,8 @@ for (i in amenities){
  	for (j in 1:length(approaches)){
  		cutoffs = all_cutoffs[[j]]
  		
-		clusts = rep(length(cutoffs[[i]]) + 1, nrow(amen))
-		for (k in 1:nrow(amen)){
-			for (p in 1:length(cutoffs[[i]])) {
-				if (amen[k,i] < cutoffs[[i]][p]) {  
-					clusts[k] = p
-					break
-				}
-			}
-		}
+ 		#assign cluster numbers
+		clusts = findInterval(amen[,i], cutoffs[[i]]) + 1
 	  amen[,approaches[j]] = clusts
  	}
  	
@@ -121,12 +114,22 @@ for (i in amenities){
 	  names(percentages)[1] = 'value'
 	  df_long_pop = bind_rows(percentages, df_long_pop)
 	}
-
+  #print(df_long_count)
+  #print(df_long_pop)
 	
+  #re-assign factor numbers for coloring purposes
+  max_n = max(df_long_count$value)
+  for (approach in approaches){
+    len = max(df_long_count$value[df_long_count$variable == approach])
+    df_long_count[df_long_count$variable == approach,'value'] = round(seq(1, max_n, length.out = len))
+    df_long_pop[df_long_pop$variable == approach,'value'] = round(seq(1, max_n, length.out = len))
+  }
+  
+  
 	#make plot
 	t[[1]] = ggplot(df_long_count, aes(y = variable, x = percent, fill = as.factor(value))) +
-		geom_col(position = "fill") +
-		scale_fill_manual(values = nice_colors) + 
+		geom_col(position = position_fill(reverse = TRUE)) +
+		scale_fill_manual(values = brewer.pal(max_n, "YlGnBu")) + 
 		xlab('') + ylab('') + guides(fill = 'none') + ggtitle('# of DBs') + 
     theme( 
       panel.grid.major = element_blank(), 
@@ -136,12 +139,12 @@ for (i in amenities){
       axis.ticks.y = element_blank(),
       #axis.text.y = element_blank(),
       axis.text.x = element_blank(), #removes x axis labels
-      axis.ticks.x = element_blank() #removes x axis ticks
+      #axis.ticks.x = element_blank() #removes x axis ticks
     )
 	t[[2]] = ggplot(df_long_pop, aes(y = variable, x = percentage, fill = as.factor(value))) +
-		geom_col(position = "fill") +
-		scale_fill_manual(values = nice_colors) + 
-		xlab('') + ylab('') + guides(fill = 'none') + ggtitle('Population') + 
+		geom_col(position = position_fill(reverse = TRUE)) +
+		scale_fill_manual(values = brewer.pal(max_n, "YlGnBu")) + 
+		xlab('Proportion') + ylab('') + guides(fill = 'none') + ggtitle('Population') + 
     theme( 
       panel.grid.major = element_blank(), 
       panel.grid.minor = element_blank(),
@@ -149,15 +152,16 @@ for (i in amenities){
       plot.title = element_text(size = 10),
       axis.ticks.y = element_blank(),
       #axis.text.y = element_blank(),
-      axis.text.x = element_blank(), #removes x axis labels
-      axis.ticks.x = element_blank() #removes x axis ticks
+      #axis.text.x = element_blank(), #removes x axis labels
+      #axis.ticks.x = element_blank() #removes x axis ticks
     )
 	legend_plt = ggplot(df_long_pop, aes(y = variable, x = percentage, fill = as.factor(value))) +
-		geom_col(position = "fill") + labs(fill="Cluster") +
-		scale_fill_manual(values = nice_colors) + 
-	  guides(fill = guide_legend(title.position = "top", label.hjust = 0.5)) + 
+		geom_col(position = "fill") + labs(fill=" ") +
+		scale_fill_manual(values = brewer.pal(max_n, "YlGnBu"), labels=c('Least Proximal', as.character(2:(max_n-1)), 'Most Proximal')) + 
+	  guides(fill = guide_legend(title.position = "top", label.hjust = 0.5, nrow=1)) + 
 		theme(
-        legend.direction = "horizontal"
+        legend.direction = "horizontal",
+        legend.position = "bottom"
 		) 
 	
 	#arrange in grid
@@ -165,13 +169,19 @@ for (i in amenities){
 
 	layout_mat = rbind(c(1),
                     c(1),
+                    c(1),
+                    #c(1),
                     c(2),
                     c(2),
+                    c(2),
+                    c(2),
+                    #c(2),
+                    #c(3),
                     c(3) )
 	barp = do.call(grid.arrange,list(grobs=t, layout_matrix=layout_mat,top=labs[counter]))
 	
 	#export
-	ggsave(paste0(labs[counter], "barplot.png"), barp, dpi = 400, width=8, height=5)
+	ggsave(paste0(labs[counter], "_barplot.png"), barp, dpi = 400, width=8, height=5)
   counter = counter + 1
 }
 
